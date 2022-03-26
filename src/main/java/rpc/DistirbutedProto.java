fda5e744d9be32d2960690d3338db9649814a382
DESKTOP-45BL147\Garace <kwstasfilios@gmail.com>
2022-03-26 21:04:33 +0200

    proto changes

--- src/main/java/rpc/DistirbutedProto.java
+++ src/main/java/rpc/DistirbutedProto.java
@@ -3,10 +3,11 @@ package rpc;
 import io.grpc.stub.StreamObserver;
 import io.grpc.unipi.distributed.APIResponse;
 import io.grpc.unipi.distributed.DistributeAuthenticationGrpc;
+import io.grpc.unipi.distributed.UserName;
 
 public class DistirbutedProto extends DistributeAuthenticationGrpc.DistributeAuthenticationImplBase {
     @Override
-    public void hello(io.grpc.unipi.distributed.UserName request, StreamObserver<APIResponse> responseObserver) {
-        super.hello(request, responseObserver);
+    public void authLogin(UserName request, StreamObserver<APIResponse> responseObserver) {
+        super.authLogin(request, responseObserver);
     }
 }
