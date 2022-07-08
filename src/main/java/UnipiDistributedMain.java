4469212e69348d4c13e5afc792c07b0799615ec1
kostas filios <>
2022-07-08 17:11:58 +0300

    add hello world

--- src/main/java/UnipiDistributedMain.java
+++ src/main/java/UnipiDistributedMain.java
@@ -1,6 +1,10 @@
 import restfull.UnipiDistributedRestFullRouter;
 
+import static spark.Spark.port;
+
 public class UnipiDistributedMain {
+
+    // Default url: http://localhost:4567
     public static void main(String[] args) {
         UnipiDistributedRestFullRouter restRouter = new UnipiDistributedRestFullRouter();
         restRouter.startRouting();
--- src/main/java/restfull/controllers/login/LoginController.java
+++ src/main/java/restfull/controllers/login/LoginController.java
@@ -3,7 +3,7 @@ package restfull.controllers.login;
 import com.google.gson.Gson;
 import conformers.RoutingControllerConformer;
 
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 public class LoginController implements RoutingControllerConformer {
 
@@ -13,6 +13,9 @@ public class LoginController implements RoutingControllerConformer {
         post("/auth/login", (req, res) -> {
             LoginAuthRequest test = mGson.fromJson(req.body(), LoginAuthRequest.class);
             return "hey post";
+        });
+        get("/hello", (req, res) -> {
+            return "hello";
         });
     }
 }
