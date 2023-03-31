220adf330afea96e16ce114818eaf77bb69147a8
kostas filios <>
2023-03-31 15:23:55 +0300

    minor logging changes

--- src/main/java/UnipiDistributedMain.java
+++ src/main/java/UnipiDistributedMain.java
@@ -1,12 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rest.UnipiDistributedRestRouter;
 import rpc.UnipiDistributedRpcRouter;
 
 public class UnipiDistributedMain {
     public static void main(String[] args) {
-
-        UnipiDistributedRpcRouter rpcRouter = new UnipiDistributedRpcRouter();
-        rpcRouter.startRouting();
         UnipiDistributedRestRouter restRouter =  new UnipiDistributedRestRouter();
         restRouter.startRouting();
+        UnipiDistributedRpcRouter rpcRouter = new UnipiDistributedRpcRouter();
+        rpcRouter.startRouting();
     }
 }
--- src/main/java/rest/UnipiDistributedRestRouter.java
+++ src/main/java/rest/UnipiDistributedRestRouter.java
@@ -2,10 +2,12 @@ package rest;
 
 import conformers.RoutingControllerConformer;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static spark.Spark.get;
 
 public class UnipiDistributedRestRouter implements RoutingControllerConformer {
-
     //default Spark port is 4567
     @Override
     public void startRouting() {
